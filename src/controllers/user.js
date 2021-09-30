6ccc1f64ffd6a9e4c545b840781ac225264f7bfa
Gandha-PC <ganzs499@gmail.com>
2021-10-01 00:52:32 +0700

    Add Users

---
+++ src/controllers/user.js
@@ -0,0 +1,127 @@
+const { user } = require('../../models');
+
+exports.addUsers = async (req, res) => {
+  try {
+    await user.create(req.body);
+
+    res.send({
+      status: 'success',
+      message: 'Add user finished',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await user.findAll({
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      status: 'success',
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await user.findAll({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      status: 'success',
+      data: {
+        user: data,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await user.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      status: 'success',
+      message: `Update user id: ${id} finished`,
+      data: req.body,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  // code here
+  try {
+    const { id } = req.params;
+
+    // if(id !== user.id) {
+    //   return res.send({
+    //     status: "failed",
+    //     message: `User with id ${id} not found`
+    //   })
+    // }
+
+    await user.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      status: 'success',
+      message: `Delete user id: ${id} finished`,
+      data: {
+        id,
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
